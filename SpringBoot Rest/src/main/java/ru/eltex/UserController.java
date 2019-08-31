ce10b284460ffdff1a80f59f39a38556f2800d14
Favarish <Favarish@yandex.ru>
2019-08-31 10:01:43 +0700

    add example simple test for RestController

--- SpringBoot Rest/src/main/java/ru/eltex/UserController.java
+++ SpringBoot Rest/src/main/java/ru/eltex/UserController.java
@@ -5,16 +5,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+
 @RestController
 public class UserController {
-    @RequestMapping("/get_user")
-    public String getUser() {
-        //System.out.println(id);
-        return "indexStr";
+    @RequestMapping("/get_users")
+    public LinkedList<User> getUser() {
+        LinkedList<User> list = new LinkedList<>();
+        list.add(new User(3, "Kory", "555"));
+        list.add(new User(4, "Teilor", "666"));
+        return list;
     }
-    @RequestMapping("/get_user/{id}")
-    public User getUser1(@PathVariable("id") Integer id) {
-        System.out.println(id);
+
+    @RequestMapping("/sendQuery/{id}/{fio}")
+    public User getUser1(@PathVariable("id") Integer id, @PathVariable("fio") String fio) {
+        System.out.println("Вот что пришло с веба: " + id + " " + fio);
         return new User(1, "Koks", "456798132");
+    }
+
+    @RequestMapping("get_information")
+    public LinkedList<User> getUsers() {
+        LinkedList<User> list = new LinkedList<>();
+        list.add(new User(3, "Kory", "555"));
+        list.add(new User(4, "Teilor", "666"));
+        list.add(new User(5, "Penny", "222"));
+
+        return list;
     }
 }
